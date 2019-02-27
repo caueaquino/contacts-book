0c33402765167f9c433d6bc43d865589e98c664f
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2019-02-26 22:13:37 -0300

    added component top and side bar to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
